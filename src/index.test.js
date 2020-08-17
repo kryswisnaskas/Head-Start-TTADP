e93e95f582d9b48afefc9b42e7ee818ced32c599
Josh Salisbury <josh@adhocteam.us>
2020-08-17 11:01:49 -0500

    fix test

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ import app from './app';
 
 describe('Root', () => {
   test('Returns a 200', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/api');
     expect(response.status).toBe(200);
   });
 });
